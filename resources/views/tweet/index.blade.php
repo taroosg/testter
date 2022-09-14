91e9ce6ec68c1748a3d86a1bdd8a853603a2f6c9
taroosg <root@taroosg.dev>
2022-09-14 12:06:34 +0900

    update index

--- resources/views/tweet/index.blade.php
+++ resources/views/tweet/index.blade.php
@@ -22,9 +22,11 @@
               <tr class="hover:bg-grey-lighter">
                 <td class="py-4 px-6 border-b border-grey-light">
                   <a href="{{ route('tweet.show',$tweet->id) }}">
+                    <p class="text-left text-grey-dark">{{$tweet->user->name}}</p>
                     <h3 class="text-left font-bold text-lg text-grey-dark">{{$tweet->tweet}}</h3>
                   </a>
                   <div class="flex">
+                    @if ($tweet->user_id === Auth::user()->id)
                     <!-- 更新ボタン -->
                     <form action="{{ route('tweet.edit',$tweet->id) }}" method="GET" class="text-left">
                       @csrf
@@ -44,6 +46,7 @@
                         </svg>
                       </button>
                     </form>
+                    @endif
                   </div>
                 </td>
               </tr>
