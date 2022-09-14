d60f29ed699aa500ecaf1f2b0fe51e4a9d579dc1
taroosg <root@taroosg.dev>
2022-09-14 12:19:20 +0900

    add tweet_user table and relation

--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -24,4 +24,9 @@ class Tweet extends Model
   {
     return $this->belongsTo(User::class);
   }
+
+  public function users()
+  {
+    return $this->belongsToMany(User::class)->withTimestamps();
+  }
 }
